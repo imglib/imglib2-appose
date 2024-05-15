a0736a3b7861df0270394fa0563809ec6f857829
tpietzsch <tobias.pietzsch@gmail.com>
2024-05-15 17:14:48 +0200

    javadoc, explore more examples

--- src/main/java/net/imglib2/appose/NDArrayUtils.java
+++ src/main/java/net/imglib2/appose/NDArrayUtils.java
@@ -4,6 +4,7 @@ import static org.apposed.appose.NDArray.Shape.Order.F_ORDER;
 
 import java.util.Objects;
 
+import org.apposed.appose.DType;
 import org.apposed.appose.NDArray;
 
 import net.imglib2.img.array.ArrayImg;
@@ -11,16 +12,67 @@ import net.imglib2.img.basictypeaccess.nio.BufferAccess;
 import net.imglib2.img.basictypeaccess.nio.BufferDataAccessFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.NativeTypeFactory;
+import net.imglib2.util.Cast;
 import net.imglib2.util.Fraction;
 
 public class NDArrayUtils
 {
+	/**
+	 * Allocate an Appose {@link NDArray} with {@link DType} corresponding to
+	 * the given ImgLib2 {@code type}.
+	 *
+	 * @param type
+	 * 		ImgLib2 type of {@code NDArray} elements
+	 * @param dimensions
+	 * 		dimensions of the {@code NDArray} (in F-prder, as used by ImgLib2)
+	 * @param <T>
+	 * 		element type
+	 *
+	 * @return a new {@code NDArray}
+	 *
+	 * @throws IllegalArgumentException
+	 * 		If dimensions are too large for the data to fit in an {@code
+	 *      ArrayImg}, or if {@code type} has no corresponding {@code DType}.
+	 */
 	public static < T extends NativeType< T > > NDArray ndArray( final T type, final int... dimensions )
 	{
 		return new NDArray( DTypeUtils.dtype( type ), new NDArray.Shape( F_ORDER, dimensions ) );
 	}
 
-	static < T extends NativeType< T >, A extends BufferAccess< A > > ArrayImg< T, A > asArrayImg(
+	/**
+	 * Wrap an Appose {@link NDArray} as an {@code ArrayImg} with matching type.
+	 *
+	 * @param ndArray
+	 * 		the Appose {@link NDArray} to wrap.
+	 * @param <T>
+	 * 		pixel type
+	 *
+	 * @return ArrayImg wrapping {@code ndArray}
+	 */
+	public static < T extends NativeType< T > > ArrayImg< T, ? > asArrayImg( final NDArray ndArray )
+	{
+		return asArrayImg( ndArray, Cast.unchecked( DTypeUtils.type( ndArray.dType() ).get() ) );
+	}
+
+	/**
+	 * Wrap an Appose {@link NDArray} as an {@code ArrayImg<T>}. The specified
+	 * {@code T type} must have underlying {@code PrimitiveType} that fits the
+	 * {@code ndArray} data type.
+	 *
+	 * @param ndArray
+	 * 		the Appose {@link NDArray} to wrap.
+	 * @param type
+	 * 		instance of the pixel type
+	 * @param <T>
+	 * 		pixel type
+	 * @param <A>
+	 *
+	 * @return ArrayImg wrapping {@code ndArray}
+	 *
+	 * @throws IllegalArgumentException
+	 * 		if type doesnt match ndArray type
+	 */
+	public static < T extends NativeType< T >, A extends BufferAccess< A > > ArrayImg< T, A > asArrayImg(
 			final NDArray ndArray,
 			final T type )
 	{
@@ -29,7 +81,6 @@ public class NDArrayUtils
 				DTypeUtils.primitiveType( ndArray.dType() ) ) )
 			throw new IllegalArgumentException();
 
-//		final long[] dimensions = ndArray.shape().to( F_ORDER ).toLongArray();
 		final long[] dimensions = ndArray.shape().toLongArray( F_ORDER );
 		final Fraction entitiesPerPixel = type.getEntitiesPerPixel();
 		@SuppressWarnings( { "unchecked", "rawtypes" } )
---
+++ src/main/java/net/imglib2/appose/SharedMemoryImg.java
@@ -0,0 +1,210 @@
+package net.imglib2.appose;
+
+import java.util.function.Supplier;
+
+import org.apposed.appose.NDArray;
+
+import net.imglib2.Cursor;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.WrappedImg;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.stream.LocalizableSpliterator;
+import net.imglib2.type.NativeType;
+import net.imglib2.util.ImgUtil;
+import net.imglib2.util.Util;
+
+/**
+ * Wraps an {@code ArrayImg} backed by Appose {@link NDArray}.
+ * <p>
+ * {@code RandomAccessibleInterval} methods are forwarded to the {@code ArrayImg}.
+ * <p>
+ * Use {@link #ndArray} to get the wrapped Appose {@link NDArray}.
+ *
+ * @param <T>
+ */
+public class SharedMemoryImg< T extends NativeType< T > > implements RandomAccessibleInterval< T >, WrappedImg< T >, Img< T >
+{
+	private final NDArray ndArray;
+
+	private final ArrayImg< T, ? > img;
+
+	/**
+	 * Copy the given {@code RandomAccessibleInterval} into a new {@link SharedMemoryImg}.
+	 */
+	public static < T extends NativeType< T > > SharedMemoryImg< T > copyOf( RandomAccessibleInterval< T > rai )
+	{
+		final SharedMemoryImg< T > copy = new SharedMemoryImg<>( rai.getType(), Util.long2int( rai.dimensionsAsLongArray() ) );
+		ImgUtil.copy( rai, copy );
+		return copy;
+	}
+
+	/**
+	 *
+	 * @param typeSupplier
+	 * @param dimensions
+	 * @throws IllegalArgumentException
+	 * 		if dimensions are too large for the data to fit in an {@code ArrayImg}
+	 */
+	public SharedMemoryImg( final Supplier< T > typeSupplier, final int... dimensions  )
+	{
+		this( typeSupplier.get(), dimensions );
+	}
+
+	private SharedMemoryImg( final T type, final int... dimensions )
+	{
+		this( type, NDArrayUtils.ndArray( type, dimensions ) );
+	}
+
+	private SharedMemoryImg( final T type, final NDArray ndArray )
+	{
+		this.ndArray = ndArray;
+		this.img = NDArrayUtils.asArrayImg( ndArray, type );
+	}
+
+	/**
+	 * Get the {@code NDArray} storing the image data.
+	 *
+	 * @return the wrapped {@code NDArray}.
+	 */
+	public NDArray ndArray()
+	{
+		return ndArray;
+	}
+
+	// -- WrappedImg ----------------------------------------------------------
+
+	@Override
+	public ArrayImg< T, ? > getImg()
+	{
+		return img;
+	}
+
+	// -- RandomAccessibleInterval --------------------------------------------
+
+	@Override
+	public T getType()
+	{
+		return img.getType();
+	}
+
+	@Override
+	public int numDimensions()
+	{
+		return img.numDimensions();
+	}
+
+	@Override
+	public long min( final int d )
+	{
+		return img.min( d );
+	}
+
+	@Override
+	public long max( final int d )
+	{
+		return img.max( d );
+	}
+
+	@Override
+	public long dimension( final int d )
+	{
+		return img.dimension( d );
+	}
+
+	@Override
+	public RandomAccess< T > randomAccess()
+	{
+		return img.randomAccess();
+	}
+
+	@Override
+	public RandomAccess< T > randomAccess( final Interval interval )
+	{
+		return img.randomAccess( interval );
+	}
+
+	@Override
+	public Cursor< T > cursor()
+	{
+		return img.cursor();
+	}
+
+	@Override
+	public Cursor< T > localizingCursor()
+	{
+		return img.localizingCursor();
+	}
+
+	@Override
+	public LocalizableSpliterator< T > spliterator()
+	{
+		return img.spliterator();
+	}
+
+	@Override
+	public LocalizableSpliterator< T > localizingSpliterator()
+	{
+		return img.localizingSpliterator();
+	}
+
+	@Override
+	public long size()
+	{
+		return img.size();
+	}
+
+	@Override
+	public Object iterationOrder()
+	{
+		return img.iterationOrder();
+	}
+
+
+	// -- Img -----------------------------------------------------------------
+
+	@Override
+	public ImgFactory< T > factory()
+	{
+		return new SharedMemoryImgFactory<>( getType() );
+	}
+
+	@Override
+	public Img< T > copy()
+	{
+		return copyOf( this );
+	}
+
+	private static class SharedMemoryImgFactory< T extends NativeType< T > > extends ImgFactory< T >
+	{
+		SharedMemoryImgFactory( T type )
+		{
+			super( type );
+		}
+
+		@Override
+		public Img< T > create( final long... dimensions )
+		{
+			return new SharedMemoryImg<>( type(), Util.long2int( dimensions ) );
+		}
+
+		@Override
+		public < S > ImgFactory< S > imgFactory( final S type ) throws IncompatibleTypeException
+		{
+			if ( type instanceof NativeType )
+				return new SharedMemoryImgFactory( ( NativeType ) type );
+			throw new IncompatibleTypeException( this, type.getClass().getCanonicalName() + " does not implement NativeType." );
+		}
+
+		@Override
+		@Deprecated
+		public Img< T > create( final long[] dim, final T type )
+		{
+			return new SharedMemoryImg<>( type, Util.long2int( dim ) );
+		}
+	}
+}
