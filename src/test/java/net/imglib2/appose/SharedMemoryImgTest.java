d1f5ce29ba6e8ac2a0fb172120f9af61a0413248
Curtis Rueden <ctrueden@wisc.edu>
2025-01-15 17:48:46 -0600

    Simplify the unit tests
    The preprocessing logic makes the routines more complex to understand.
    And I don't want to imply that you can put an Img into a Map and then
    feed that to Appose. You have to pass an NDArray object. So the tests
    now showcase the different ways of doing that.

--- src/test/java/net/imglib2/appose/SharedMemoryImgTest.java
+++ src/test/java/net/imglib2/appose/SharedMemoryImgTest.java
@@ -32,13 +32,13 @@ package net.imglib2.appose;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.real.FloatType;
 import org.apposed.appose.Appose;
 import org.apposed.appose.Environment;
 import org.apposed.appose.NDArray;
 import org.apposed.appose.Service;
 import org.apposed.appose.Service.Task;
+import org.apposed.appose.Service.TaskStatus;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -54,31 +54,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static net.imglib2.appose.NDArrayUtils.asNDArray;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Create an Img, pass it through Appose, and print it in Python as NumPy ndarray.
- * <p>
- * Various possible variants. Not sure all of them are good ideas.
- * </p>
+ * Tests shared access to N-dimensional images between processes via Appose.
  */
 public class SharedMemoryImgTest
 {
-	private static final String PRINT_INPUT =
-			"import numpy as np\n" +
-			"task.outputs['result'] = str(img.ndarray())";
-
 	private static Service python;
 
 	@BeforeAll
 	public static void setUp() throws IOException
 	{
 		// Read environment.yml from test resources.
-		InputStream is = SharedMemoryImgTest.class.getResourceAsStream("environment.yml");
+		InputStream is = SharedMemoryImgTest.class.getResourceAsStream( "environment.yml" );
 		assertNotNull( is );
-		String envYaml = new BufferedReader( new InputStreamReader(is, StandardCharsets.UTF_8) )
-			.lines().collect( Collectors.joining("\n") );
+		String envYaml = new BufferedReader( new InputStreamReader( is, StandardCharsets.UTF_8 ) )
+			.lines().collect( Collectors.joining( "\n" ) );
 
 		// Build an environment with Python + Appose + NumPy available.
 		python = Appose.include( envYaml, "environment.yml" ).logDebug().build().python();
@@ -92,13 +84,13 @@ public class SharedMemoryImgTest
 	}
 
 	/**
-	 * We create a NDArray then wrap it into an ArrayImg.
+	 * We create an {@link NDArray} then wrap it into an {@code ArrayImg}.
 	 * <p>
-	 * We have to keep track of the NDArray to pass it to Appose.
+	 * We keep track of the {@code NDArray} so that we can pass it to Appose.
 	 * </p>
 	 */
 	@Test
-	public void version1() throws Exception
+	public void ndArrayToPython() throws Exception
 	{
 		final FloatType type = new FloatType();
 		try ( final NDArray ndArray = NDArrayUtils.ndArray( type, 4, 3, 2 ) )
@@ -109,102 +101,57 @@ public class SharedMemoryImgTest
 			for ( FloatType t : img )
 				t.set( i++ );
 
-			final Map< String, Object > inputs = new HashMap<>();
-			inputs.put( "img", ndArray );
-			List< String > actual = doTask( python, PRINT_INPUT, inputs );
-			List< String > expected = Arrays.asList(
-				"[[[ 0.  1.  2.  3.]",
-				"  [ 4.  5.  6.  7.]",
-				"  [ 8.  9. 10. 11.]]",
-				"",
-				" [[12. 13. 14. 15.]",
-				"  [16. 17. 18. 19.]",
-				"  [20. 21. 22. 23.]]]"
-			);
-			assertEquals( expected, actual );
+			assertAccessibleFromPython( ndArray );
 		}
 	}
 
 	/**
-	 * We create SharedMemoryImg, which creates and wraps a NDArray internally.
+	 * We create a {@link SharedMemoryImg}, which creates and wraps an
+	 * {@link NDArray} internally.
 	 * <p>
-	 * Less cumbersome, but we need to refer to it as SharedMemoryImg to be able
-	 * to get the .ndArray() out for passing it to Appose.
+	 * We refer to it as a {@code SharedMemoryImg} (rather than a plain
+	 * {@code Img}) so that we can invoke the {@code .ndArray()} method for
+	 * passing the wrapped {@code NDArray} to Appose.
 	 * </p>
 	 */
 	@Test
-	public void version2() throws Exception
+	public void shmImgToPython() throws Exception
 	{
-		final SharedMemoryImg< FloatType > img = new SharedMemoryImg<>( new FloatType(), 4, 3, 2 );
+		try ( final SharedMemoryImg< FloatType > img = new SharedMemoryImg<>( new FloatType(), 4, 3, 2 ) )
+		{
+			int i = 0;
+			for ( FloatType t : img )
+				t.set( i++ );
 
-		int i = 0;
-		for (FloatType t : img)
-			t.set(i++);
-
-		final Map< String, Object > inputs = new HashMap<>();
-		inputs.put( "img", img.ndArray());
-		doTask( python, PRINT_INPUT, inputs );
+			assertAccessibleFromPython( img.ndArray() );
+		}
 	}
 
 	/**
-	 * We create SharedMemoryImg, which creates and wraps a NDArray internally.
+	 * We create a {@link SharedMemoryImg}, which creates and wraps an
+	 * {@link NDArray} internally.
 	 * <p>
-	 * We use it as Img and put it into the Appose inputs directly.
-	 * </p>
-	 * <p>
-	 * Then we have to preprocess() inputs to find SharedMemoryImgs and replace
-	 * them by the wrapped NDArrays.
+	 * We use it as {@link Img} and extract its {@link NDArray} using
+	 * {@link NDArrayUtils#asNDArray} so that we can pass it to Appose.
 	 * </p>
 	 */
 	@Test
-	public void version3() throws Exception
+	public void implicitShmImgToPython() throws Exception
 	{
 		final Img< FloatType > img = new SharedMemoryImg<>( new FloatType(), 4, 3, 2 );
 
 		int i = 0;
-		for (FloatType t : img)
-			t.set(i++);
+		for ( FloatType t : img )
+			t.set( i++ );
 
-		final Map< String, Object > inputs = new HashMap<>();
-		inputs.put( "img", img);
-		doTask( python, PRINT_INPUT, preprocess3(inputs) );
+		assertAccessibleFromPython( NDArrayUtils.asNDArray( img, false ) );
 	}
 
 	/**
-	 * We create a "normal" ArrayImg (or any other RandomAccessibleInterval),
-	 * and it into the Appose inputs directly.
-	 * <p>
-	 * Then we have to preprocess() inputs to
-	 * </p>
-	 * <ul>
-	 *     <li>find SharedMemoryImgs and replace them by the wrapped NDArrays,
-	 *         and</li>
-	 *     <li>find any other RandomAccessibleInterval and copy its data into a
-	 *         new NDArray.</li>
-	 * </ul>
-	 * <p>
-	 * It is convenient, but maybe it would be better to make explicit copying
-	 * necessary?
-	 * </p>
-	 */
-	@Test
-	public void version4() throws Exception
-	{
-		final Img< FloatType > img = ArrayImgs.floats( 4, 3, 2 );
-
-		int i = 0;
-		for (FloatType t : img)
-			t.set(i++);
-
-		final Map< String, Object > inputs = new HashMap<>();
-		inputs.put( "img", img);
-		doTask( python, PRINT_INPUT, preprocess4(inputs) );
-	}
-
-	/**
-	 * We create a "normal" ArrayImg (or any other RandomAccessibleInterval),
-	 * and put it into the Appose inputs using {@link
-	 * NDArrayUtils#asNDArray(RandomAccessibleInterval)}.
+	 * We create a "normal" ArrayImg (or any other
+	 * {@link RandomAccessibleInterval}) not backed by shared memory, then copy it
+	 * into an {@link NDArray} using {@link NDArrayUtils#asNDArray} so that we can
+	 * pass it to Appose.
 	 * <p>
 	 * This works with {@link SharedMemoryImg} and other
 	 * {@link RandomAccessibleInterval}s, both. It either extracts the wrapped
@@ -212,21 +159,19 @@ public class SharedMemoryImgTest
 	 * </p>
 	 */
 	@Test
-	public void version5() throws Exception
+	public void nonShmImgToPython() throws Exception
 	{
 		final Img< FloatType > img = ArrayImgs.floats( 4, 3, 2 );
 
 		int i = 0;
-		for (FloatType t : img)
-			t.set(i++);
+		for ( FloatType t : img )
+			t.set( i++ );
 
-		final Map< String, Object > inputs = new HashMap<>();
-		inputs.put( "img", asNDArray(img) );
-		doTask( python, PRINT_INPUT, inputs );
+		assertAccessibleFromPython( NDArrayUtils.asNDArray( img ) );
 	}
 
 	/**
-	 * Create an Img, pass it through Appose as an NDArray, wrap it as a
+	 * Creates an {@link Img}, passes it through Appose as an NDArray, wrap it as a
 	 * SharedMemoryImg on the other end.
 	 */
 	@Test
@@ -240,27 +185,26 @@ public class SharedMemoryImgTest
 
 		// Pass our same classpath to the Groovy worker.
 		final List< String > classpath = Arrays.asList(
-			System.getProperty( "java.class.path" ).split("[:;]")
+			System.getProperty( "java.class.path" ).split( "[:;]" )
 		);
 		Environment env = Appose.system();
 		try ( Service service = env.groovy( classpath ) )
 		{
 			final Map< String, Object > inputs = new HashMap<>();
-			inputs.put( "ndarray", asNDArray( img ) );
+			inputs.put( "ndarray", NDArrayUtils.asNDArray( img ) );
 			String script =
 				"import net.imglib2.appose.SharedMemoryImg\n" +
 				"SharedMemoryImg img = new SharedMemoryImg(ndarray)\n" +
-				"return img.collect { it.get() }\n";
-			Service.Task task = service.task( script, inputs );
+				"img.collect { it.get() }\n";
+			Task task = service.task( script, inputs );
 			task.waitFor();
-			assertSame( Service.TaskStatus.COMPLETE, task.status, task.error );
+			assertSame( TaskStatus.COMPLETE, task.status, task.error );
 			Object result = task.outputs.get( "result" );
 			assertInstanceOf( List.class, result );
-			@SuppressWarnings("unchecked")
-			List< Object > values = ( List< Object > ) result;
+			List< ? > values = ( List< ? > ) result;
 			for ( int i = 0; i < values.size(); i++ )
 			{
-				Object value = values.get(i);
+				Object value = values.get( i );
 				assertInstanceOf( Number.class, value );
 				Number number = ( Number ) value;
 				assertEquals( i, number.floatValue() );
@@ -268,13 +212,24 @@ public class SharedMemoryImgTest
 		}
 	}
 
-	private List< String > doTask(Service service, String script, Map< String, Object > inputs ) throws IOException, InterruptedException
+	private void assertAccessibleFromPython( final NDArray data ) throws IOException, InterruptedException
 	{
-		Task task = service.task( script, inputs );
+		final String printInput =
+			"import numpy as np\n" +
+			"task.outputs['datatype'] = str(type(data))\n" +
+			"task.outputs['result'] = str(data.ndarray())";
+
+		final Map< String, Object > inputs = new HashMap<>();
+		inputs.put( "data", data );
+
+		Task task = python.task( printInput, inputs );
 		task.waitFor();
-		assertSame( Service.TaskStatus.COMPLETE, task.status, task.error );
+
+		assertSame( TaskStatus.COMPLETE, task.status, task.error );
+		Object dataType = task.outputs.get( "datatype" );
+		assertEquals( "<class 'appose.types.NDArray'>", dataType );
 		String result = ( String ) task.outputs.get( "result" );
-		List< String > actual = Arrays.asList( result.split("(\r\n|\n|\r)") );
+		List< String > actual = Arrays.asList( result.split( "(\r\n|\n|\r)" ) );
 		List< String > expected = Arrays.asList(
 			"[[[ 0.  1.  2.  3.]",
 			"  [ 4.  5.  6.  7.]",
@@ -285,30 +240,5 @@ public class SharedMemoryImgTest
 			"  [20. 21. 22. 23.]]]"
 		);
 		assertEquals( expected, actual );
-		return actual;
-	}
-
-	private static Map< String, Object > preprocess3( final Map< String, Object > inputs )
-	{
-		inputs.entrySet().forEach( entry -> {
-			final Object value = entry.getValue();
-			if ( value instanceof SharedMemoryImg )
-				entry.setValue( ( ( SharedMemoryImg< ? > ) value ).ndArray() );
-		} );
-		return inputs;
-	}
-
-	private static Map< String, Object > preprocess4( final Map< String, Object > inputs )
-	{
-		inputs.entrySet().forEach( entry -> {
-			final Object value = entry.getValue();
-			if ( value instanceof RandomAccessibleInterval )
-			{
-				final RandomAccessibleInterval rai = ( RandomAccessibleInterval ) value;
-				if ( rai.getType() instanceof NativeType )
-					entry.setValue( asNDArray( rai, true ) );
-			}
-		} );
-		return inputs;
 	}
 }
