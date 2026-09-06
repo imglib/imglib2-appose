21a494cc5b8e08a7cbd6cc560874c877bdef117f
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-09-06 16:32:49 +0200

    WIP: A version of the pixi runner lets caller change parameters between runs.

---
+++ src/main/java/net/imglib2/appose/util/AbstractPixiRunner2.java
@@ -0,0 +1,163 @@
+package net.imglib2.appose.util;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apposed.appose.Appose;
+import org.apposed.appose.BuildException;
+import org.apposed.appose.Environment;
+import org.apposed.appose.Service;
+import org.apposed.appose.Service.Task;
+import org.apposed.appose.Service.TaskStatus;
+import org.apposed.appose.TaskException;
+
+public class AbstractPixiRunner2 implements AutoCloseable
+{
+
+	private final String envName;
+
+	private final URL pythonRunScriptPath;
+
+	private final URL pythonInitScriptPath;
+
+	private final URL pixiTomlPath;
+
+	private final URL pythonUtilScriptPath;
+
+	private final ApposeTaskListener listener;
+
+	private Service python;
+
+	private String runScript;
+
+	protected AbstractPixiRunner2(
+			final URL pixiTomlPath,
+			final URL pythonUtilScriptPath,
+			final URL pythonInitScriptPath,
+			final URL pythonRunScriptPath,
+			final String envName,
+			final ApposeTaskListener listener )
+	{
+		this.pixiTomlPath = pixiTomlPath;
+		this.pythonUtilScriptPath = pythonUtilScriptPath;
+		this.pythonInitScriptPath = pythonInitScriptPath;
+		this.pythonRunScriptPath = pythonRunScriptPath;
+		this.envName = envName;
+		this.listener = listener;
+	}
+
+	/**
+	 * Initialize the runner.
+	 * <p>
+	 * This involves creating the Python environment, running the utility
+	 * script, and running the initialization script.
+	 * 
+	 * @param inputsParams
+	 *            the input parameters to pass to the Python initialization
+	 *            script.
+	 *
+	 * @throws IOException
+	 *             if there is an error reading the initialization or run
+	 *             scripts or the pixi.toml file.
+	 * @throws BuildException
+	 *             if there is an error building the Python environment.
+	 * @throws InterruptedException
+	 *             if the thread is interrupted while waiting for the
+	 *             initialization task to complete.
+	 * @throws TaskException
+	 *             if there is an error running the Python initialization task.
+	 * @throws RuntimeException
+	 *             if the Python initialization task fails with an error.
+	 */
+	protected void init( final Map< String, Object > inputsParams ) throws IOException, BuildException, InterruptedException, TaskException
+	{
+		// Create Python env.
+		final String pixiTomlContent = IOUtils.toString( pixiTomlPath, StandardCharsets.UTF_8 );
+		final Environment env = Appose
+				.pixi()
+				.content( pixiTomlContent )
+				.subscribeProgress( listener.progressListener() )
+				.subscribeOutput( listener.outputListener() )
+				.subscribeError( listener.errorListener() )
+				.build();
+		final String pythonUtilScript = ( pythonUtilScriptPath == null )
+				? ""
+				: IOUtils.toString( pythonUtilScriptPath, StandardCharsets.UTF_8 );
+		this.python = env.activate( envName ).python().init( pythonUtilScript );
+
+		// The Python initialization task.
+		if ( pythonInitScriptPath != null )
+		{
+			final String pythonInitScript = IOUtils.toString( pythonInitScriptPath, StandardCharsets.UTF_8 );
+			final Task task = python.task( pythonInitScript, inputsParams );
+
+			final long start = System.currentTimeMillis();
+			// To catch update message from the python script
+			task.listen( listener.taskListener() );
+			task.start();
+			// Wait for task completion.
+			task.waitFor();
+
+			// Verify that it worked.
+			if ( task.status != TaskStatus.COMPLETE )
+				throw new RuntimeException( "Python script failed with error: " + task.error );
+
+			// Benchmark.
+			final long end = System.currentTimeMillis();
+			listener.message( "Initialization done in " + ( end - start ) / 1000. + " s" );
+		}
+		// Load it now and only once.
+		this.runScript = IOUtils.toString( pythonRunScriptPath, StandardCharsets.UTF_8 );
+	}
+
+	/**
+	 * Execute the Python run script.
+	 * 
+	 * @param inputsParams
+	 *            the input parameters to pass to the Python run script.
+	 *
+	 * @throws InterruptedException
+	 *             if the thread is interrupted while waiting for the run task
+	 *             to complete.
+	 * @throws TaskException
+	 *             if there is an error running the Python run task.
+	 * @throws IllegalStateException
+	 *             if the runner has not been initialized by calling
+	 *             {@link #init()} before calling this method.
+	 * @throws RuntimeException
+	 *             if the Python run task fails with an error.
+	 */
+	protected void run( final Map< String, Object > inputsParams ) throws InterruptedException, TaskException
+	{
+		if ( runScript == null )
+			throw new IllegalStateException( "You must call init() before calling run()" );
+
+		// The Python task.
+		final Task task = python.task( runScript, inputsParams );
+
+		final long start = System.currentTimeMillis();
+		// To catch update message from the python script
+		task.listen( listener.taskListener() );
+		task.start();
+		// Wait for task completion.
+		task.waitFor();
+
+		// Verify that it worked.
+		if ( task.status != TaskStatus.COMPLETE )
+			throw new RuntimeException( "Python script failed with error: " + task.error );
+
+		// Benchmark.
+		final long end = System.currentTimeMillis();
+		listener.message( "Python done in " + ( end - start ) / 1000. + " s" );
+	}
+
+	@Override
+	public void close()
+	{
+		if ( python != null )
+			python.close();
+	}
+}
